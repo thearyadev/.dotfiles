28332dfd2a4f5234064a45139719aa05d7906670
Aryan Kothari <=>
2024-03-01 20:36:02 -0500

    add server conf

---
+++ machines/server/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+    
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit inputs;};
+          modules = [ 
+            ./configuration.nix
+            inputs.home-manager.nixosModules.default
+          ];
+        };
+      homeConfigurations = {
+	arya = home-manager.lib.homeManagerConfiguration {
+		inherit pkgs;
+		modules = [ "/home/arya/.dotfiles/machines/server/home.nix" ];
+	};
+      };
+
+
+    };
+}
---
+++ machines/server/packages.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+let
+  configPath = "~/.dotfiles/machines/development/";
+in 
+{
+  home.packages = [
+    pkgs.nixfmt
+    pkgs.htop
+    pkgs.fish
+    pkgs.neovim
+    pkgs.gcc
+    (pkgs.writeShellScriptBin "nixos-flake-switch" ''
+      sudo nixos-rebuild switch --flake ./#default --impure
+    '')
+
+    (pkgs.writeShellScriptBin "home-manager-flake-switch" ''
+      home-manager switch --flake . --impure
+    '')
+
+    (pkgs.writeShellScriptBin "home-build" ''
+      cd ${configPath} && home-manager-flake-switch
+    '')
+    (pkgs.writeShellScriptBin "nixos-build" ''
+      cd ${configPath} && nixos-flake-switch 
+    '')
+
+    (pkgs.writeShellScriptBin "conf" ''
+      cd ${configPath} && nvim
+    '')
+
+
+  ];
+}
