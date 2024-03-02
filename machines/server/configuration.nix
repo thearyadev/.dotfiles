d6eee8dd2e55f1b816a465f58d821182535a95d0
Aryan Kothari <=>
2024-03-02 14:08:06 -0500

    fix routing

--- machines/server/configuration.nix
+++ machines/server/configuration.nix
@@ -36,7 +36,7 @@ in
 
   services.tailscale = {
     enable = true;
-    #useRoutingFeatures = "server";
+    useRoutingFeatures = "server";
     extraUpFlags = [ "--advertise-exit-node" "--advertise-routes=192.168.1.0/24"];
   };
   # Configure network proxy if necessary
