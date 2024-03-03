2c639dd730d0cfdce149cc0a01da2d073e6c5556
Aryan Kothari <87589047+thearyadev@users.noreply.github.com>
2024-03-02 20:38:00 -0500

    init fish with prisma vars

--- machines/development/home.nix
+++ machines/development/home.nix
@@ -15,5 +15,12 @@
     ./packages.nix
     ./dotfiles.nix
   ]; 
+  pkgs.fish.shellInit = ``
+      export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
+      export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
+      export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
+      export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
+
+  ``
   programs.home-manager.enable = true;
 } 
